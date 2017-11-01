a45176f152332a6a0c9f1cfbcf5f1b47e0a1b330
maxice <maxice@gmail.com>
2017-11-01 07:15:31 +0530

    Material Search

--- src/app/view.go
+++ src/app/view.go
@@ -10,8 +10,42 @@ import (
 	"github.com/fragmenta/view"
 
 	"github.com/bitcubate/cryptojournal/src/lib/helpers"
+    "crypto/md5"
+	"encoding/hex"
+	"io"
+	"strings"
 )
 
+// This function takes in an email address as a parameter and returns a complete url to the image.
+// for example: GetImg("example@example.com") will return this: http://www.gravatar.com/avatar/563dd2c62e66d42a3b4454c410a0b3ab?d=identicon
+func GetImg(email string) string {
+	email = strings.TrimSpace(email)
+	email = strings.ToLower(email)
+
+	h := md5.New()
+	io.WriteString(h, email)
+
+	finalBytes := h.Sum(nil)
+
+	finalString := "http://www.gravatar.com/avatar/" + hex.EncodeToString(finalBytes) + "?d=retro"
+
+	return finalString
+}
+
+// This function takes in an email address as a parameter and returns just the hash, which you can use to your liking.
+func GetHash(email string) string {
+	email = strings.TrimSpace(email)
+	email = strings.ToLower(email)
+
+	h := md5.New()
+	io.WriteString(h, email)
+
+	finalBytes := h.Sum(nil)
+
+	finalString := hex.EncodeToString(finalBytes)
+
+	return finalString
+}
 // SetupAssets compiles or copies our assets from src into the public assets folder.
 func SetupAssets() {
 	defer log.Time(time.Now(), log.V{"msg": "Finished loading assets"})
