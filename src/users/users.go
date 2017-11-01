282663954206bc77e009312f37c2bf99ec013c43
maxice <maxice@gmail.com>
2017-11-01 07:17:53 +0530

    Material Search

--- src/users/users.go
+++ src/users/users.go
@@ -6,7 +6,42 @@ import (
 
 	"github.com/bitcubate/cryptojournal/src/lib/resource"
 	"github.com/bitcubate/cryptojournal/src/lib/status"
+    "crypto/md5"
+	"encoding/hex"
+	"io"
+	"strings"
 )
+
+// This function takes in an email address as a parameter and returns a complete url to the image.
+// for example: GetImg("example@example.com") will return this: http://www.gravatar.com/avatar/563dd2c62e66d42a3b4454c410a0b3ab?d=identicon
+func GetImg(email string) string {
+	email = strings.TrimSpace(email)
+	email = strings.ToLower(email)
+
+	h := md5.New()
+	io.WriteString(h, email)
+
+	finalBytes := h.Sum(nil)
+
+	finalString := "http://www.gravatar.com/avatar/" + hex.EncodeToString(finalBytes) + "?d=retro"
+
+	return finalString
+}
+
+// This function takes in an email address as a parameter and returns just the hash, which you can use to your liking.
+func GetHash(email string) string {
+	email = strings.TrimSpace(email)
+	email = strings.ToLower(email)
+
+	h := md5.New()
+	io.WriteString(h, email)
+
+	finalBytes := h.Sum(nil)
+
+	finalString := hex.EncodeToString(finalBytes)
+
+	return finalString
+}
 
 // User handles saving and retreiving users from the database
 type User struct {
